8742c4747255a44188e0edeced9f9b519b786fa1
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-04-06 19:56:34 -0300

    terminando exercicio

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
+import File (descompactarArquivo, compactarArquivo)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    -- Compacta o arquivo
+    putStrLn "Compactando arquivo..."
+    compactarArquivo "teste.txt"
+    putStrLn "Arquivo compactado com sucesso!"
+    
+    -- Descompacta o arquivo
+    putStrLn "Descompactando arquivo..."
+    descompactarArquivo "teste.huff"
+    putStrLn "Arquivo descompactado com sucesso!"
+    
+
+
