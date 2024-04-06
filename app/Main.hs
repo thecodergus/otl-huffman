5dfbb6c570714065b5faf53e266f9ffc4d143b8a
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-04-06 20:39:40 -0300

    Atual estado

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,33 @@
 module Main where
-import File (descompactarArquivo, compactarArquivo)
+-- import File (descompactarArquivo, compactarArquivo)
+import Algorithms
+import Algorithms (codificar)
 
 main :: IO ()
 main = do
-    -- Compacta o arquivo
-    putStrLn "Compactando arquivo..."
-    compactarArquivo "teste.txt"
-    putStrLn "Arquivo compactado com sucesso!"
-    
-    -- Descompacta o arquivo
-    putStrLn "Descompactando arquivo..."
-    descompactarArquivo "teste.huff"
-    putStrLn "Arquivo descompactado com sucesso!"
-    
 
+    -- Codificando e decodificando com Huffman
+    let texto = "Gustavo Michels de Camargo"
 
+    let arvore = construirArvore $ freqSimb texto
+
+    let textoCompactado = codificar texto $ arvore
+
+    let textoDecodificado = decodificar textoCompactado $ arvore
+
+    putStrLn $ "Texto original: " ++ texto
+
+    putStrLn $ "Texto compactado: " ++ textoCompactado
+
+    putStrLn $ "Texto decodificado: " ++ textoDecodificado
+
+    
+    -- -- Compacta o arquivo
+    -- putStrLn "Compactando arquivo..."
+    -- compactarArquivo "teste.txt"
+    -- putStrLn "Arquivo compactado com sucesso!"
+    
+    -- -- Descompacta o arquivo
+    -- putStrLn "Descompactando arquivo..."
+    -- descompactarArquivo "teste.txt.huff"
+    -- putStrLn "Arquivo descompactado com sucesso!"
