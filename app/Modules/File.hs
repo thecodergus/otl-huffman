493bb26731dd80fe4302057f14b39d785f62e02a
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-04-15 18:02:15 -0300

    Comentando o codigo

--- app/Modules/File.hs
+++ app/Modules/File.hs
@@ -17,7 +17,18 @@ import Data.Map (Map)
 
 
 
-encode :: String -> IO ()
+-- | Codifica um arquivo usando o algoritmo de Huffman.
+--
+-- A função `encode` recebe o caminho de um arquivo como entrada e realiza a codificação do mesmo usando o algoritmo de Huffman.
+-- O arquivo é lido, e em seguida são calculadas as frequências dos símbolos presentes no texto.
+-- A árvore de Huffman é construída a partir das frequências dos símbolos.
+-- Em seguida, o arquivo binário é gerado com base nas informações da árvore de Huffman e das frequências dos símbolos.
+-- O arquivo binário é salvo com a extensão ".bin" no mesmo diretório do arquivo original.
+--
+-- A função `encode` é uma função de entrada e saída (IO) que não retorna nenhum valor.
+-- Ela utiliza as funções auxiliares `somarCaracteres`, `escreverArquivo`, `escreverSimbolos` e `escreverCodigos` para realizar a codificação.
+encode :: String -- ^ O caminho do arquivo a ser codificado.
+       -> IO ()  -- ^ Ação IO que realiza a codificação do arquivo.
 encode arquivo = do
   texto <- readFile arquivo
   let freqSimbolos = freqSimb texto
@@ -28,12 +39,18 @@ encode arquivo = do
   Lazy.writeFile (arquivo ++ ".bin") arquivoBin
 
     where
+      -- | Calcula a soma das frequências dos símbolos presentes na lista de Huffman.
       somarCaracteres :: [Huffman] -> Int
       somarCaracteres [] = 0
       somarCaracteres ((Folha freq _):hs) = freq + somarCaracteres hs
       somarCaracteres ((No freq _ _):hs) = freq + somarCaracteres hs
 
-      escreverArquivo :: Int -> Int -> [Huffman] -> Huffman -> Put
+      -- | Escreve as informações do arquivo binário com base nas frequências dos símbolos e na árvore de Huffman.
+      escreverArquivo :: Int         -- ^ O total de frequências de símbolos.
+                     -> Int         -- ^ O total de caracteres no texto original.
+                     -> [Huffman]   -- ^ A lista de Huffman.
+                     -> Huffman     -- ^ A árvore de Huffman.
+                     -> Put         -- ^ Ação Put que escreve as informações do arquivo binário.
       escreverArquivo totalFrequenciaSimbolos totalCaracteres [] arvore = do
         Put.putWord8 $ fromIntegral totalFrequenciaSimbolos
         Put.putWord32be $ fromIntegral totalCaracteres
@@ -46,7 +63,9 @@ encode arquivo = do
 
         escreverSimbolos simbolos
           
-      escreverSimbolos :: [Huffman] -> Put
+      -- | Escreve as informações dos símbolos no arquivo binário.
+      escreverSimbolos :: [Huffman] -- ^ A lista de Huffman.
+                       -> Put       -- ^ Ação Put que escreve as informações dos símbolos.
       escreverSimbolos [] = return ()
       escreverSimbolos ((Folha freq simbolo):hs) = do
         Put.putWord8 $ fromIntegral $ ord simbolo
@@ -55,7 +74,10 @@ encode arquivo = do
       escreverSimbolos ((No {}):hs) = do
         escreverSimbolos hs
 
-      escreverCodigos :: Huffman -> Map Char ByteString -> Put
+      -- | Escreve os códigos dos símbolos no arquivo binário.
+      escreverCodigos :: Huffman              -- ^ A árvore de Huffman.
+                      -> Map Char ByteString -- ^ O mapa de códigos dos símbolos.
+                      -> Put                  -- ^ Ação Put que escreve os códigos dos símbolos.
       escreverCodigos (Folha _ simbolo) codigos = do
         let codigo = Map.findWithDefault (error "Símbolo não encontrado") simbolo codigos
         Put.putWord8 $ fromIntegral $ ord simbolo
